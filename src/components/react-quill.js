072502763c9d61b040fb15e23523fc3ce707d532
afeiship <1290657123@qq.com>
2018-08-30 17:54:50 +0800

    issue: add feature btns

--- src/components/react-quill.js
+++ src/components/react-quill.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
@@ -33,14 +33,15 @@ export default class extends Component {
 
   componentDidMount() {
     const { value, options } = this.props;
-    this.quill = new Quill(this.root, options);
+    objectAssign(options, { modules: { toolbar: this.toolbar } });
+    this.quill = new Quill(this.container, options);
     this.quill.on('editor-change', this._onEditorChange);
     this.html = value;
   }
 
-  componentWillReceiveProps(inProps){
+  componentWillReceiveProps(inProps) {
     const { value } = inProps;
-    if( value !== this.html ){
+    if (value !== this.html) {
       this.html = value;
     }
   }
@@ -58,7 +59,54 @@ export default class extends Component {
   render() {
     const { className, ...props } = this.props;
     return (
-      <section ref={(root) => this.root = root} className={classNames('react-quill', className)} {...props} />
+      <Fragment>
+        <div ref={(toolbar) => this.toolbar = toolbar}>
+          <span className="ql-formats">
+            <select className="ql-font"></select>
+            <select className="ql-size"></select>
+          </span>
+          <span className="ql-formats">
+            <button className="ql-bold"></button>
+            <button className="ql-italic"></button>
+            <button className="ql-underline"></button>
+            <button className="ql-strike"></button>
+          </span>
+          <span className="ql-formats">
+            <select className="ql-color"></select>
+            <select className="ql-background"></select>
+          </span>
+          <span className="ql-formats">
+            <button className="ql-script" value="sub"></button>
+            <button className="ql-script" value="super"></button>
+          </span>
+          <span className="ql-formats">
+            <button className="ql-header" value="1"></button>
+            <button className="ql-header" value="2"></button>
+            <button className="ql-blockquote"></button>
+            <button className="ql-code-block"></button>
+          </span>
+          <span className="ql-formats">
+            <button className="ql-list" value="ordered"></button>
+            <button className="ql-list" value="bullet"></button>
+            <button className="ql-indent" value="-1"></button>
+            <button className="ql-indent" value="+1"></button>
+          </span>
+          <span className="ql-formats">
+            <button className="ql-direction" value="rtl"></button>
+            <select className="ql-align"></select>
+          </span>
+          <span className="ql-formats">
+            <button className="ql-link"></button>
+            <button className="ql-image"></button>
+            <button className="ql-video"></button>
+            <button className="ql-formula"></button>
+          </span>
+          <span className="ql-formats">
+            <button className="ql-clean"></button>
+          </span>
+        </div>
+        <section ref={(container) => this.container = container} className={classNames('react-quill', className)} {...props} />
+      </Fragment>
     );
   }
 }
